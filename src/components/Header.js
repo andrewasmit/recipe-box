c15e7ce85fe63b97e6eae3faeb94fd6cf5337962
andrew <andrewasmit@hotmail.com>
2022-12-17 08:48:54 -0600

    added components to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Search from "./Search";
+import Filter from "./Filter";
 
 function Header(){
     return(
         <div className="App-header">
             <h1>Recipe Box</h1>
             <h3>The Place to Keep your Food Blogs</h3>
+            <Search />
+            <Filter />
         </div>
     )
 }
---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Search(){
+    return(
+        <div id = "search-bar">
+            <label>Search Recipes</label>
+            <input type ="text"></input>
+        </div>
+    )
+}
+
+export default Search;
