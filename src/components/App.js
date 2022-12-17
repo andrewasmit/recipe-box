1d600ad8d572a1d21c2b73b7f5720425478dade8
andrew <andrewasmit@hotmail.com>
2022-12-16 20:30:31 -0600

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import '../App.css';
+import Header from './Header';
+
+function App() {
+  
+
+// Return JSX
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header(){
+    return(
+        <div className="App-header">
+            <h1>Recipe Box</h1>
+            <h3>The Place to Keep your Food Blogs</h3>
+        </div>
+    )
+}
+
+export default Header;
