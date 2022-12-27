aa3d9c2e31c7031bb7416d861d51c1812eb70ca9
Andrew <andrewasmit@hotmail.com>
2022-12-27 08:43:55 -0600

    updated navbar to color and center properly

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,6 @@ import { NavLink } from "react-router-dom";
 function NavBar(){
     return(
         <div id="navbar">
-            <h3>THIS IS THE NAVBAR</h3>
             <NavLink to="/" >Home</NavLink>
             <NavLink to="/recipe-box">My Recipe Box</NavLink>
             <NavLink to="/recipes">Search New Recipes</NavLink>
