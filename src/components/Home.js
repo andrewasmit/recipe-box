5f0c8cda3ca74e179c2255a34b50e16ccb454931
Andrew <andrewasmit@hotmail.com>
2022-12-28 20:47:22 -0600

    added 3 main photos for Home page and am processing the carousel feature

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,20 @@ import React from "react";
 
 function Home(){
     return(
-        <div id ="home" className="container">
-            <p>HOME PAGE</p>
+        <div id ="home" >
+            <div id="home-slider">
+                <div className="slide" id="slide-1">
+                    <img src="1main.jpg" alt="cooking picture" />
+                </div>
+                <div className="slide" id="slide-2">
+                    <img src="2main.jpg" alt="cooking picture" />
+                </div>
+                <div className="slide" id="slide-3">
+                    <img src="3main.jpg" alt="cooking picture" />
+                </div>
+                <button className="slider-btn" id="prev-slide">Previous</button>
+                <button className="slider-btn"id="next-slide">Next</button>
+            </div>
         </div>
     )
 }
