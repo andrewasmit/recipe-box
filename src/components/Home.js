7521f6030e8fde6b3c99f3a7b9f9029ce5ce3a9f
andrew <andrewasmit@hotmail.com>
2022-12-29 13:13:57 -0600

    carousel is sized correctly and is navigating properly!

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,46 +1,66 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState } from "react";
 
 function Home(){
 
+    const [currentSlide, setCurrentSlide] = useState(0);
+    // const [slides, setSlides] = useState=([]);
+
+    // Event Handler for Carousel
+    function handleSlideClick(e){
+        console.log(e.target.id)
+        if (e.target.id === "next-slide"){
+            handleNextSlide()
+        }
+        if (e.target.id === "prev-slide"){
+            handlePrevSlide()
+        }
+    }
+
+    function handleNextSlide(){
+        if (currentSlide===2){
+            setCurrentSlide(0)
+        }
+        if (currentSlide<2){
+            setCurrentSlide(currentSlide+1);
+        }
+    }
+
+    function handlePrevSlide(){
+        if (currentSlide===0){
+            setCurrentSlide(2)
+        }
+        if (currentSlide>0){
+            setCurrentSlide(currentSlide-1);
+        }        
+    }
+
+    // useEffect to grab each slide after rendering and then define their X-Axis
     useEffect(()=>{
         const slides = document.getElementsByClassName('slide')
         console.log(slides);
         Array.from(slides).forEach((slide, idx) => {
-            slide.style.transform = `translateX(${idx * 100}%)`;
+            slide.style.transform = `translateX(${100 * (idx - currentSlide)}%)`;
           });
+    },[currentSlide]);
 
-          let curSlide = 0;
-
-// select next slide button
-const nextSlide = document.getElementById("next-slide");
-
-// add event listener and next slide functionality
-nextSlide.addEventListener("click", function () {
-     curSlide++;
-
-  slides.forEach((slide, indx) => {
-    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
-  });
-});
-
-    },[]);
-
+   
+console.log(currentSlide);
 
     return(
         <div id ="home" >
             <h2>WELCOME</h2>
             <div id="home-slider">
                 <div className="slide" id="slide-1">
-                    <img src="1main.jpg" alt="cooking picture" />
+                    <img src="1main.jpg" alt="cooking" />
                 </div>
                 <div className="slide" id="slide-2">
-                    <img src="2main.jpg" alt="cooking picture" />
+                    <img src="2main.jpg" alt="cooking" />
                 </div>
                 <div className="slide" id="slide-3">
-                    <img src="3main.jpg" alt="cooking picture" />
+                    <img src="3main.jpg" alt="cooking" />
                 </div>
-                <button className="slider-btn" id="prev-slide">Previous</button>
-                <button className="slider-btn"id="next-slide">Next</button>
+                <button className="slider-btn" id="prev-slide"onClick={handleSlideClick}> ← </button>
+                <button className="slider-btn"id="next-slide" onClick={handleSlideClick}> → </button>
             </div>
         </div>
     )
