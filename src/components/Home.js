ad0f54d3dc92bb29a1e4409fd562f4de7a92d249
Andrew <andrewasmit@hotmail.com>
2022-12-29 12:35:08 -0600

    update to home page carousel

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,31 @@
-import React from "react";
-
+import React, {useEffect} from "react";
 
 function Home(){
+
+    useEffect(()=>{
+        const slides = document.getElementsByClassName('slide')
+        console.log(slides);
+        Array.from(slides).forEach((slide, idx) => {
+            slide.style.transform = `translateX(${idx * 100}%)`;
+          });
+
+          let curSlide = 0;
+
+// select next slide button
+const nextSlide = document.getElementById("next-slide");
+
+// add event listener and next slide functionality
+nextSlide.addEventListener("click", function () {
+     curSlide++;
+
+  slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+  });
+});
+
+    },[]);
+
+
     return(
         <div id ="home" >
             <h2>WELCOME</h2>
